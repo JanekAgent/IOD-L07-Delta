9cff7ca4a71313944f3addd6e9ed94785361451c
Janek_Agent <janek.skowron@outlook.com>
2023-12-20 21:50:02 +0100

    v0.4.1 Refactor code structure and remove unused classes

--- src/main/java/pl/put/poznan/buildinginfo/logic/Building.java
+++ src/main/java/pl/put/poznan/buildinginfo/logic/Building.java
@@ -1,10 +1,10 @@
 package pl.put.poznan.buildinginfo.logic;
 
 
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+
+
 
 public class Building {
 
